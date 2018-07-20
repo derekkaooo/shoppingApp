695e5f511c0453da356a7d424930f10055ca7941
Derek <stiau334@gmail.com>
2018-07-20 16:40:03 +0800

    Initial Commit

---
+++ shoppingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shoppingApp
+//
+//  Created by Derek on 2018/7/20.
+//  Copyright © 2018年 Derek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
